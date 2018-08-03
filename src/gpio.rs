8221b0f0b32df7b8fb9b3fad06c6489aea20f503
kaojo <mail@kaojo.de>
2018-08-03 20:59:10 +0200

    added the possibility to toggle a valve pin via a gpio input

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,51 +1,44 @@
-use sysfs_gpio;
+use sysfs_gpio::{Direction, Edge, Error, Pin};
 
 #[derive(Clone)]
 pub struct PinLayout {
-    power_pin: sysfs_gpio::Pin,
-    error_pin: sysfs_gpio::Pin,
-    valve_pins: Vec<sysfs_gpio::Pin>,
+    power_pin: Pin,
+    error_pin: Pin,
+    valve_pins: Vec<ToggleValve>,
 }
 
 impl PinLayout {
-    pub fn new(power_pin_num: u64, error_pin_num: u64, valve_pin_nums: Vec<u64>) -> PinLayout {
-        let power_pin = sysfs_gpio::Pin::new(power_pin_num);
+    pub fn new(
+        power_pin_num: u64,
+        error_pin_num: u64,
+        toggle_valves: Vec<ToggleValve>,
+    ) -> PinLayout {
+        let power_pin = Pin::new(power_pin_num);
         power_pin.export().expect("GPIO error.");
         power_pin
-            .set_direction(sysfs_gpio::Direction::Out)
+            .set_direction(Direction::Out)
             .expect("GPIO error.");
 
-        let error_pin = sysfs_gpio::Pin::new(error_pin_num);
+        let error_pin = Pin::new(error_pin_num);
         error_pin.export().expect("GPIO error.");
         error_pin
-            .set_direction(sysfs_gpio::Direction::Out)
+            .set_direction(Direction::Out)
             .expect("GPIO error.");
-
-        let mut valve_pins = Vec::with_capacity(valve_pin_nums.len());
-        for valve_num in valve_pin_nums {
-            let valve = sysfs_gpio::Pin::new(valve_num);
-            valve.export().expect("GPIO error.");
-            valve
-                .set_direction(sysfs_gpio::Direction::Out)
-                .expect("GPIO error.");
-
-            valve_pins.push(valve);
-        }
 
         PinLayout {
             power_pin: power_pin,
             error_pin: error_pin,
-            valve_pins: valve_pins,
+            valve_pins: toggle_valves,
         }
     }
 
-    pub fn get_power_pin(&self) -> &sysfs_gpio::Pin {
+    pub fn get_power_pin(&self) -> &Pin {
         &self.power_pin
     }
-    pub fn get_error_pin(&self) -> &sysfs_gpio::Pin {
+    pub fn get_error_pin(&self) -> &Pin {
         &self.error_pin
     }
-    pub fn get_valve_pins(&self) -> Vec<&sysfs_gpio::Pin> {
+    pub fn get_valve_pins(&self) -> Vec<&ToggleValve> {
         let mut refs = Vec::with_capacity(self.valve_pins.len());
         for i in &self.valve_pins {
             refs.push(i);
@@ -53,15 +46,54 @@ impl PinLayout {
         refs
     }
 
-    pub fn unexport_all(&self) -> Result<(), sysfs_gpio::Error> {
+    pub fn unexport_all(&self) -> Result<(), Error> {
         self.power_pin.set_value(0)?;
         self.power_pin.unexport()?;
         self.error_pin.set_value(0)?;
         self.error_pin.unexport()?;
-        for v in &self.valve_pins {
+        for toggle_valve in &self.valve_pins {
+            let v = toggle_valve.get_valve_pin();
             v.set_value(0)?;
             v.unexport()?;
+            let b = toggle_valve.get_button_pin();
+            b.unexport()?;
         }
         Ok(())
+    }
+}
+
+#[derive(Clone)]
+pub struct ToggleValve {
+    valve_pin: Pin,
+    button_pin: Pin,
+}
+
+impl ToggleValve {
+    pub fn new(valve_pin_num: u64, button_pin_num: u64) -> ToggleValve {
+        let valve_pin = Pin::new(valve_pin_num);
+        valve_pin.export().expect("GPIO error.");
+        valve_pin
+            .set_direction(Direction::Out)
+            .expect("GPIO error.");
+
+        let button_pin = Pin::new(button_pin_num);
+        button_pin.export().expect("GPIO error.");
+        button_pin.set_edge(Edge::RisingEdge).expect("Edge set.");
+        button_pin
+            .set_direction(Direction::In)
+            .expect("GPIO error.");
+
+        ToggleValve {
+            valve_pin: valve_pin,
+            button_pin: button_pin,
+        }
+    }
+
+    pub fn get_valve_pin(&self) -> &Pin {
+        &self.valve_pin
+    }
+
+    pub fn get_button_pin(&self) -> &Pin {
+        &self.button_pin
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 mod gpio;
 
 extern crate ctrlc;
+extern crate futures;
 extern crate sysfs_gpio;
+extern crate tokio_core;
 
-use gpio::PinLayout;
+use futures::{Future, Stream};
+use gpio::{PinLayout, ToggleValve};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
+use sysfs_gpio::Error;
+use tokio_core::reactor::{Core, Handle};
 
-fn run_start_sequence(pin_layout: &PinLayout) -> Result<(), sysfs_gpio::Error> {
+fn run_start_sequence(pin_layout: &PinLayout) -> Result<(), Error> {
     println!("Garden buttler starting ...");
     for millis in [200, 200, 400, 200, 200].iter() {
         let running_led = pin_layout.get_power_pin();
@@ -18,14 +23,14 @@ fn run_start_sequence(pin_layout: &PinLayout) -> Result<(), sysfs_gpio::Error> {
         running_led.set_value(1)?;
         error_led.set_value(1)?;
         for v in &valves {
-            v.set_value(1)?;
+            v.get_valve_pin().set_value(1)?;
         }
 
         sleep(Duration::from_millis(*millis));
         running_led.set_value(0)?;
         error_led.set_value(0)?;
         for v in &valves {
-            v.set_value(0)?;
+            v.get_valve_pin().set_value(0)?;
         }
 
         sleep(Duration::from_millis(200));
@@ -35,14 +40,32 @@ fn run_start_sequence(pin_layout: &PinLayout) -> Result<(), sysfs_gpio::Error> {
     Ok(())
 }
 
-fn power_on(layout: &PinLayout) -> Result<(), sysfs_gpio::Error> {
+fn power_on(layout: &PinLayout) -> Result<(), Error> {
     layout.get_power_pin().set_value(1)?;
     Ok(())
 }
 
+fn stream_button_presses(handle: &Handle, layout: &PinLayout) -> Result<(), Error> {
+    for toggle_valve in &layout.get_valve_pins() {
+        let button_pin = toggle_valve.get_button_pin();
+        let valve_pin = toggle_valve.get_valve_pin().clone();
+        handle.spawn(
+            button_pin
+                .get_value_stream(&handle)?
+                .for_each(move |_val| {
+                    let new_val = 1 - valve_pin.get_value()?;
+                    valve_pin.set_value(new_val)?;
+                    Ok(())
+                })
+                .map_err(|_| ()),
+        );
+    }
+    Ok(())
+}
+
 fn main() {
-    let layout = PinLayout::new(23, 17, vec![27]);
-    
+    let layout = PinLayout::new(23, 17, vec![ToggleValve::new(27, 22)]);
+
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
     let layout_clone = layout.clone();
@@ -55,10 +78,13 @@ fn main() {
     }).expect("Ctrl-C handler");
 
     run_start_sequence(&layout).expect("StartSequence run.");
-    power_on(&layout).expect("Power Pin set to 1;");
+    power_on(&layout).expect("Power Pin turned on.");
 
+    let mut l = Core::new().expect("Tokio Core created.");
+    let handle = l.handle();
+    stream_button_presses(&handle, &layout).expect("Stream button presses.");
     while running.load(Ordering::SeqCst) {
-
+        l.turn(None);
     }
 
     println!("Exiting garden buttler ...");
