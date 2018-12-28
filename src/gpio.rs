d94ab9765116650eba965c8b83485ca357fc8b83
Julian Winter <julian.winter@dm.de>
2018-12-28 21:15:51 +0100

    refactored code

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,10 +1,15 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+use futures::{Future, lazy, Stream};
+use futures::Lazy;
 use sysfs_gpio::{Direction, Edge, Error, Pin};
 
 #[derive(Clone)]
 pub struct PinLayout {
     power_pin: Pin,
     error_pin: Pin,
-    valve_pins: Vec<ToggleValve>,
+    toggle_valves: Vec<ToggleValve>,
 }
 
 impl PinLayout {
@@ -26,21 +31,66 @@ impl PinLayout {
             .expect("GPIO error.");
 
         PinLayout {
-            power_pin: power_pin,
-            error_pin: error_pin,
-            valve_pins: toggle_valves,
+            power_pin,
+            error_pin,
+            toggle_valves,
         }
     }
 
-    pub fn get_power_pin(&self) -> &Pin {
-        &self.power_pin
+    pub fn run_start_sequence(&self) -> Result<(), Error> {
+        for millis in [200, 200, 400, 200, 200].iter() {
+            let running_led = self.power_pin;
+            let error_led = self.error_pin;
+            let valves = self.get_valve_pins();
+            running_led.set_value(1)?;
+            error_led.set_value(1)?;
+            for v in &valves {
+                v.get_valve_pin().set_value(1)?;
+            }
+
+            sleep(Duration::from_millis(*millis));
+            running_led.set_value(0)?;
+            error_led.set_value(0)?;
+            for v in &valves {
+                v.get_valve_pin().set_value(0)?;
+            }
+
+            sleep(Duration::from_millis(200));
+        }
+
+        Ok(())
     }
-    pub fn get_error_pin(&self) -> &Pin {
-        &self.error_pin
+
+    pub fn power_on(&self) -> Result<(), Error> {
+        self.power_pin.set_value(1)?;
+        Ok(())
     }
-    pub fn get_valve_pins(&self) -> Vec<&ToggleValve> {
-        let mut refs = Vec::with_capacity(self.valve_pins.len());
-        for i in &self.valve_pins {
+
+    pub fn get_button_streams(&self) -> impl Future<Item = (), Error = ()> {
+        let clone = self.clone();
+        return lazy(move || {
+            for toggle_valve in clone.get_valve_pins() {
+                let button_pin = toggle_valve.get_button_pin();
+                let valve_pin = toggle_valve.get_valve_pin().clone();
+                tokio::spawn(
+                    button_pin
+                        .get_value_stream().expect("Expect a valid value stream.")
+                        .for_each(move |_val| {
+                            let new_val = 1 - valve_pin.get_value()?;
+                            valve_pin.set_value(new_val)?;
+                            Ok(())
+                        }).map_err(|err| {
+                        println!("error = {:?}", err)
+                    }),
+                );
+            }
+            Ok(())
+        });
+    }
+
+    fn get_valve_pins(&self) -> Vec<&ToggleValve> {
+        let mut refs = Vec::with_capacity(self.toggle_valves.len());
+        for i in &self.toggle_valves {
             refs.push(i);
         }
         refs
@@ -51,7 +101,7 @@ impl PinLayout {
         self.power_pin.unexport()?;
         self.error_pin.set_value(0)?;
         self.error_pin.unexport()?;
-        for toggle_valve in &self.valve_pins {
+        for toggle_valve in &self.toggle_valves {
             let v = toggle_valve.get_valve_pin();
             v.set_value(0)?;
             v.unexport()?;
@@ -84,8 +134,8 @@ impl ToggleValve {
             .expect("GPIO error.");
 
         ToggleValve {
-            valve_pin: valve_pin,
-            button_pin: button_pin,
+            valve_pin,
+            button_pin,
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -3,77 +3,27 @@ extern crate sysfs_gpio;
 extern crate tokio;
 extern crate tokio_signal;
 
-use std::thread::sleep;
-use std::time::Duration;
 
-use futures::{Future, lazy, Stream};
-use sysfs_gpio::Error;
-use tokio::reactor::Handle;
+use futures::{Future, Stream};
 use tokio::runtime::Runtime;
 
 use gpio::{PinLayout, ToggleValve};
 
 mod gpio;
 
-fn run_start_sequence(pin_layout: &PinLayout) -> Result<(), Error> {
-    println!("Garden buttler starting ...");
-    for millis in [200, 200, 400, 200, 200].iter() {
-        let running_led = pin_layout.get_power_pin();
-        let error_led = pin_layout.get_error_pin();
-        let valves = pin_layout.get_valve_pins();
-        running_led.set_value(1)?;
-        error_led.set_value(1)?;
-        for v in &valves {
-            v.get_valve_pin().set_value(1)?;
-        }
-
-        sleep(Duration::from_millis(*millis));
-        running_led.set_value(0)?;
-        error_led.set_value(0)?;
-        for v in &valves {
-            v.get_valve_pin().set_value(0)?;
-        }
-
-        sleep(Duration::from_millis(200));
-    }
-    println!("Garden buttler started ...");
-
-    Ok(())
-}
-
-fn power_on(layout: &PinLayout) -> Result<(), Error> {
-    layout.get_power_pin().set_value(1)?;
-    Ok(())
-}
-
 fn main() {
+    println!("Garden buttler starting ...");
     let layout = PinLayout::new(23, 17, vec![ToggleValve::new(27, 22)]);
     let layout_clone = layout.clone();
 
-    run_start_sequence(&layout).expect("StartSequence run.");
-    power_on(&layout).expect("Power Pin turned on.");
+    layout.run_start_sequence().expect("StartSequence run.");
+    layout.power_on().expect("Power Pin turned on.");
 
     let mut rt = Runtime::new().unwrap();
 
-    let button_streams = lazy(move || {
-        for toggle_valve in layout_clone.get_valve_pins() {
-            let button_pin = toggle_valve.get_button_pin();
-            let valve_pin = toggle_valve.get_valve_pin().clone();
-            tokio::spawn(
-                button_pin
-                    .get_value_stream().expect("Expect a valid value stream.")
-                    .for_each(move |_val| {
-                        let new_val = 1 - valve_pin.get_value()?;
-                        valve_pin.set_value(new_val)?;
-                        Ok(())
-                    }).map_err(|err| {
-                    println!("error = {:?}", err)
-                }),
-            );
-        }
-        Ok(())
-    });
+    let button_streams = layout.get_button_streams();
     rt.spawn(button_streams);
+    println!("Garden buttler started ...");
 
     // wait until program is terminated
     let ctrl_c = tokio_signal::ctrl_c().flatten_stream().take(1).map_err(|err| println!("error = {:?}", err));
