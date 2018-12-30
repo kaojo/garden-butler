64b2afaa68021f402739e282ac085da24856dd13
Julian Winter <julian.winter@dm.de>
2018-12-30 22:49:01 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,8 @@ mod embedded;
 fn main() {
     println!("Garden buttler starting ...");
 
-    let mut settings = config::Config::default();
-    settings
-        // Add in `./Layout.toml`
-        .merge(config::File::new("layout", config::FileFormat::Json)).unwrap()
-        // Add in settings from the environment (with a prefix of APP)
-        // Eg.. `GB_DEBUG=1 ./target/app` would set the `debug` key
-        .merge(config::Environment::with_prefix("LAYOUT")).unwrap();
-
-    let layout_config = settings.try_into::<LayoutConfig>().expect("Layout config contains errors");
+    let layout_config = get_layout_config();
     println!("{:?}",layout_config);
-
     let layout = PinLayout::from_config(&layout_config);
 
     layout.run_start_sequence().expect("StartSequence run.");
@@ -41,14 +32,24 @@ fn main() {
 
     // wait until program is terminated
     let ctrl_c = tokio_signal::ctrl_c().flatten_stream().take(1).map_err(|err| println!("error = {:?}", err));
-
     // Process each ctrl-c as it comes in
     let prog = ctrl_c.for_each(move |()| {
         println!("ctrl-c received!");
-        layout.unexport_all().expect("Should unexport all exported gpio pins.");
+        layout.unexport_all().expect("Unexport of GPIO pins failed.");
         Ok(())
     });
-    rt.block_on(prog).expect("Should wait until app is terminated");
+    rt.block_on(prog).expect("Error waiting until app is terminated with ctrl+c");
 
     println!("Exiting garden buttler ...");
+}
+
+fn get_layout_config() -> LayoutConfig {
+    let mut settings = config::Config::default();
+    settings
+        .merge(config::File::new("layout", config::FileFormat::Json)).unwrap()
+        // Add in settings from the environment (with a prefix of LAYOUT)
+        // Eg.. `LAYOUT_POWER=11 ./target/app` would set the `debug` key
+        .merge(config::Environment::with_prefix("LAYOUT")).unwrap();
+    let layout_config = settings.try_into::<LayoutConfig>().expect("Layout config contains errors");
+    layout_config
 }
