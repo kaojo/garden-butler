3e2132dc21cd83567b7b750a1e78365996d02b62
Julian Winter <julian.winter@dm.de>
2019-09-06 11:15:42 +0200

    Added tls connection to mqtt broker

--- src/mqtt/mod.rs
+++ src/mqtt/mod.rs
@@ -2,10 +2,14 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-use rumqtt::{MqttClient, MqttOptions, Notification, QoS, Receiver, SecurityOptions};
+use rumqtt::{MqttClient, MqttOptions, Notification, QoS, Receiver, SecurityOptions, ConnectionMethod};
 
 use mqtt::configuration::MqttConfig;
 use futures::{Stream, Async};
+
+use std::fs::File;
+use std::path::Path;
+use std::io::Read;
 
 pub mod configuration;
 
@@ -16,16 +20,22 @@ pub struct MqttSession {
 
 impl MqttSession {
     pub fn from_config(config: MqttConfig) -> Arc<Mutex<MqttSession>> {
+        let mut cert_file = File::open(config.cert_path.unwrap_or("/app/root-ca.crt".to_string())).expect("Could not open root ca for mqtt connection.");
+        let mut cert = Vec::new();
+        // read the whole file
+        cert_file.read_to_end(&mut cert).expect("Could not read cert file");
+
         let mqtt_options = MqttOptions::new(
             config.client_id,
             config.broker_hostname,
-            config.port.unwrap_or(1883))
+            config.port.unwrap_or(8883))
             .set_security_opts(
                 SecurityOptions::UsernamePassword(
                     String::from(config.username.unwrap_or("".to_string())),
                     String::from(config.password.unwrap_or("".to_string())),
                 )
-            );
+            )
+            .set_connection_method(ConnectionMethod::Tls(cert, None));
         let (client, receiver) = MqttClient::start(mqtt_options).unwrap();
         Arc::new(Mutex::new(MqttSession {
             client,
